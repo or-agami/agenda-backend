ffbe2d16a9c691c8bc43c32344ac903b21c10ee5
or-agami <oragami0@gmail.com>
2022-09-16 16:06:44 +0300

    gitignore is now working

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  //?- Local DB:
-  'dbURL': 'mongodb://localhost:27017',
-
-  //?- Remote DB:
-  // 'dbURL': 'mongodb+srv://agami:agamimongo@cluster0.ojpsmci.mongodb.net/?retryWrites=true&w=majority',
-}
